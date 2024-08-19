2139de783e2b58dcc8a15b983b8dfacef9e695e8
Samuel Escobar <samueb8@gmail.com>
2024-08-19 10:37:01 -0400

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
+import { AppRoutes } from './app/routes/AppRoutes'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <AppRoutes/>
+    </Router>
   )
 }
 
