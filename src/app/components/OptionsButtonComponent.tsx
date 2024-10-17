f6378f47532439c65086384fbecd294798dee994
Samuel Escobar <samueb8@gmail.com>
2024-10-17 08:55:59 -0400

    modified the style of options button

--- src/app/components/OptionsButtonComponent.tsx
+++ src/app/components/OptionsButtonComponent.tsx
@@ -7,7 +7,7 @@ export const OptionsButtonComponent = ({
 }) => {
   const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
   return (
-    <div id="options-button-component" className="border">
+    <div id="options-button-component" className="">
       <button
         onClick={() => setOptionsOpen(!optionsOpen)}
         className="flex flex-col justify-center items-center w-8 h-8 bg-transparent rounded focus:outline-none space-y-1"
@@ -18,7 +18,7 @@ export const OptionsButtonComponent = ({
       </button>
       {optionsOpen && (
         <div id="options-open" className="w-screen h-screen fixed inset-0 z-40 p-2" onClick={()=>{setOptionsOpen(false)}}>
-          <div id="" className="flex flex-col border border-neutral-900 size-fit justify-self-end bg-primary divide-neutral-500 divide-y rounded">
+          <div id="" className="flex flex-col border border-neutral-400 size-fit justify-self-end bg-primary divide-neutral-300 divide-y rounded">
             {settings.map((setting, index) => (
               <button key={index} className="p-2 text-left min-w-36" onClick={setting.action}>{setting.text}</button>
             ))}
