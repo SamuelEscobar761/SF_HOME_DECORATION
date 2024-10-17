01cada1024b1f5be940ff3af5dd246163fcc57ab
Samuel Escobar <samueb8@gmail.com>
2024-10-17 09:01:14 -0400

    delete folder issue solved

--- src/app/components/FolderComponent.tsx
+++ src/app/components/FolderComponent.tsx
@@ -18,9 +18,15 @@ export const FolderComponent = ({
     <div
       id="folder-component"
       className="flex justify-between py-2"
-      onClick={() => onClick(id)}
+      onClick={() => {
+        optionsIsOpen && setOptionsIsOpen(false);
+      }}
     >
-      <div id="folder-img-text-container" className="flex">
+      <div
+        id="folder-img-text-container"
+        className="flex"
+        onClick={() => onClick(id)}
+      >
         <img
           src={FolderIcon}
           alt="folder-icon"
@@ -32,20 +38,25 @@ export const FolderComponent = ({
         </p>
       </div>
       {optionsIsOpen ? (
-        <button onClick={() => {setOptionsIsOpen(false); deleteFolder(id)}} className="p-1 bg-tertiary-light rounded">
-            Eliminar
-        </button>
-      ): (
         <button
-        onClick={() => setOptionsIsOpen(!optionsIsOpen)}
-        className="flex flex-col justify-center items-center bg-transparent rounded focus:outline-none space-y-1 p-1.5"
-      >
-        <span className="block w-1 h-1 bg-black rounded-full"></span>
-        <span className="block w-1 h-1 bg-black rounded-full"></span>
-        <span className="block w-1 h-1 bg-black rounded-full"></span>
-      </button>
+          onClick={() => {
+            setOptionsIsOpen(false);
+            deleteFolder(id);
+          }}
+          className="p-1 bg-tertiary-light rounded"
+        >
+          Eliminar
+        </button>
+      ) : (
+        <button
+          onClick={() => setOptionsIsOpen(!optionsIsOpen)}
+          className="flex flex-col justify-center items-center bg-transparent rounded focus:outline-none space-y-1 p-1.5"
+        >
+          <span className="block w-1 h-1 bg-black rounded-full"></span>
+          <span className="block w-1 h-1 bg-black rounded-full"></span>
+          <span className="block w-1 h-1 bg-black rounded-full"></span>
+        </button>
       )}
-      
     </div>
   );
 };
